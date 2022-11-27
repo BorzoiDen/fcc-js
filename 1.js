da45f9799431de68e303deb5bc97ecbe1c036903
BorzoiDen <borozenetsdn@gmail.com>
2022-11-27 19:25:12 +0700

    basic js 90%

--- 1.js
+++ 1.js
@@ -1,21 +1,12 @@
-function sequentialSizes(val) {
-  let answer = "";
-  switch (val) {
-    case 1:
-    case 2:
-    case 3:
-      answer = "Low";
-      return answer;
-    case 4:
-    case 5:
-    case 6:
-      answer = "Mid";
-      return answer;
-    case 7:
-    case 8:
-    case 9:
-      answer = "High";
-      return answer;
-  }
+const myArray = [];
+let i = 10;
+do {
+  myArray.push(i);
+  i++;
+} while (i < 10) {
+    if (i < 10){
+      myArray.push(i);
+    }
 }
-sequentialSizes(1);
+console.log(i);
+console.log(myArray);
