d92916f6b7cea6cc1d77166259c1a1d43026a390
BorzoiDen <borozenetsdn@gmail.com>
2022-11-27 14:22:14 +0700

    basic js 70%

---
+++ 1.js
@@ -0,0 +1,21 @@
+function sequentialSizes(val) {
+  let answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      return answer;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      return answer;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      return answer;
+  }
+}
+sequentialSizes(1);
