85ad8de902f6c635461e4bef9875cbb4751420b9
BorzoiDen <borozenetsdn@gmail.com>
2022-11-28 20:02:09 +0700

    js 100

--- 1.js
+++ 1.js
@@ -1,12 +1,39 @@
-const myArray = [];
-let i = 10;
-do {
-  myArray.push(i);
-  i++;
-} while (i < 10) {
-    if (i < 10){
-      myArray.push(i);
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for(let i = 0; i <= contacts.length; i++){
+    if(contacts[i].firstName === name){
+      if(contacts[i].prop){
+        return contacts[i][prop];
+      }
+      return 'No such property';
     }
+    return 'No such contact';
+  }
 }
-console.log(i);
-console.log(myArray);
+lookUpProfile("Akira", "likes");
